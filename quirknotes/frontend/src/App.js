89121c5c12a7963118d59188948476b55f8b77f2
Vidhi Shah <vidhishah@Vidhis-MacBook-Air.local>
2024-02-04 23:53:24 -0500

    test push

---
+++ quirknotes/frontend/src/App.js
@@ -0,0 +1,174 @@
+import React, {useState, useEffect} from "react"
+import './App.css';
+import Dialog from "./Dialog";
+import Note from "./Note";
+
+function App() {
+
+  // -- Backend-related state --
+  const [loading, setLoading] = useState(true)
+  const [notes, setNotes] = useState(undefined)
+
+  // -- Dialog props-- 
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const [dialogNote, setDialogNote] = useState(null)
+
+  
+  // -- Database interaction functions --
+  useEffect(() => {
+    const getNotes = async () => {
+      try {
+        await fetch("http://localhost:4000/getAllNotes")
+        .then(async (response) => {
+          if (!response.ok) {
+            console.log("Served failed:", response.status)
+          } else {
+              await response.json().then((data) => {
+              getNoteState(data.response)
+          }) 
+          }
+        })
+      } catch (error) {
+        console.log("Fetch function failed:", error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    getNotes()
+  }, [])
+
+  const deleteNote = (entry) => {
+    try{
+      fetch("http://localhost:4000/deleteNote:noteId")
+      .then(async (response) => {
+        if(!response.ok){
+          console.log("Served failed:", response.status)
+        } else {
+          deleteNoteState();
+        }
+      })
+
+    } catch (error){
+      console.log("Fetch function failed:", error)
+      alert("Could not delete note")
+
+    }
+  }
+
+  const deleteAllNotes = () => {
+    // Code for DELETE all notes here
+  }
+
+  
+  // -- Dialog functions --
+  const editNote = (entry) => {
+    setDialogNote(entry)
+    setDialogOpen(true)
+  }
+
+  const postNote = () => {
+    setDialogNote(null)
+    setDialogOpen(true)
+  }
+
+  const closeDialog = () => {
+    setDialogNote(null)
+    setDialogOpen(false)
+  }
+
+  // -- State modification functions -- 
+  const getNoteState = (data) => {
+    setNotes(data)
+  }
+
+  const postNoteState = (_id, title, content) => {
+    setNotes((prevNotes) => [...prevNotes, {_id, title, content}])
+  }
+
+  const deleteNoteState = () => {
+    
+  }
+
+  const deleteAllNotesState = () => {
+    // Code for modifying state after DELETE all here
+  }
+
+  const patchNoteState = (_id, title, content) => {
+    // Code for modifying state after PATCH here
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div style={dialogOpen ? AppStyle.dimBackground : {}}>
+          <h1 style={AppStyle.title}>QuirkNotes</h1>
+          <h4 style={AppStyle.text}>The best note-taking app ever </h4>
+
+          <div style={AppStyle.notesSection}>
+            {loading ?
+            <>Loading...</>
+            : 
+            notes ?
+            notes.map((entry) => {
+              return (
+              <div key={entry._id}>
+                <Note
+                entry={entry} 
+                editNote={editNote} 
+                deleteNote={deleteNote}
+                />
+              </div>
+              )
+            })
+            :
+            <div style={AppStyle.notesError}>
+              Something has gone horribly wrong!
+              We can't get the notes!
+            </div>
+            }
+          </div>
+
+          <button onClick={postNote}>Post Note</button>
+          {notes && notes.length > 0 && 
+          <button
+              onClick={deleteAllNotes}
+              >
+              Delete All Notes
+          </button>}
+
+        </div>
+
+        <Dialog
+          open={dialogOpen}
+          initialNote={dialogNote}
+          closeDialog={closeDialog}
+          postNote={postNoteState}
+          // patchNote={patchNoteState}
+          />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+const AppStyle = {
+  dimBackground: {
+    opacity: "20%", 
+    pointerEvents: "none"
+  },
+  notesSection: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: "center"
+  },
+  notesError: {color: "red"},
+  title: {
+    margin: "0px"
+  }, 
+  text: {
+    margin: "0px"
+  }
+}
